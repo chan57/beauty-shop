fe950f50eb8d24174371d2efa369c3c1234a7506
Alexey <dis320852@gmail.com>
2023-10-03 13:58:28 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import Header from "./components/Layout/Header.vue";
+
+</script>
+
+<template>
+  <div class="container">
+
+    <Header />
+    <!-- <img src="./assets/fon.png" class="backgImg"> -->
+  </div>
+
+</template>
+
+<style lang="scss">
+@import url('https://fonts.cdnfonts.com/css/satoshi');
+@import url('https://fonts.cdnfonts.com/css/lt-edge');
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+ font-family: 'Satoshi', sans-serif;
+//  font-family: 'LT Edge', sans-serif;
+
+}
+
+.container {
+  max-width: 1440px;
+height: 900px;
+  background-image: url("./assets/fon.png");
+
+
+
+}
+</style>
---
+++ src/components/Layout/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <header class="header">
+    <div class="header-top">
+
+      <a href="#">catalog</a>
+      <a href="#">about</a>
+      <a href="#">delivery</a>
+
+
+        <div class="header-logo">
+        <img href="#" src="/svg/header-logo.svg">
+      </div>
+      <a href="#">contacts</a>
+      <div class="header-search-group">
+        <img class="r" src="/svg/header-search.svg">
+      </div>
+
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+ height: 121px;
+//  background: #F5ECE3;
+ padding: 0px 70.182px 0px 70px;
+// justify-content: center;
+// align-items: center;
+// flex-shrink: 0;
+}
+
+.header-top {
+//   font-size: 14px;
+// font-style: normal;
+// font-weight: 400;
+// line-height: 120%; /* 16.8px */
+// letter-spacing: -0.28px;
+
+display: flex;
+justify-content: space-between;
+
+}
+
+.header-search-group {
+
+}
+
+.r {
+  width: 110px;
+height: 17px;
+
+}
+</style>
